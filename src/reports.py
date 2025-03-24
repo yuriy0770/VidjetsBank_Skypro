6787647d9680e46420edc16b9bdba1b202980acf
origin <yura28049119@yandex.ru>
2025-03-24 17:33:48 +0300

    исправление ошибок

--- src/reports.py
+++ src/reports.py
@@ -1,18 +1,22 @@
-import pandas as pd
 from datetime import datetime, timedelta
 from typing import Optional
 
+import pandas as pd
 
 
 def wrapper(f):
     def inner(*args):
-        with open(r"C:\Users\User\skypro_project1\report\report.txt", "w", encoding="utf-8") as file:
+        with open(
+            r"C:\Users\User\skypro_project1\report\report.txt", "w", encoding="utf-8"
+        ) as file:
             file.write(str(f(*args)))
+
     return inner
 
 
 @wrapper
 def spending_by_category(file_path: str, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    '''Функция возвращает траты по заданной категории за последние три месяца (от переданной даты).'''
     try:
         df = pd.read_excel(file_path)
         if date is None:
@@ -20,24 +24,31 @@ def spending_by_category(file_path: str, category: str, date: Optional[str] = No
         else:
             current_date = pd.to_datetime(date).date()
         start_date = current_date - timedelta(days=90)
-        filtered_transactions = df[(df['Категория'] == category) &
-                                          (df['Дата операции'] >= start_date.strftime('%Y-%m-%d')) &
-                                          (df['Дата операции'] <= current_date.strftime('%Y-%m-%d'))]
+        filtered_transactions = df[
+            (df["Категория"] == category)
+            & (df["Дата операции"] >= start_date.strftime("%Y-%m-%d"))
+            & (df["Дата операции"] <= current_date.strftime("%Y-%m-%d"))
+        ]
 
-        grouped_transactions = filtered_transactions.groupby(['Дата операции', 'Сумма платежа']).sum().reset_index()
+        grouped_transactions = (
+            filtered_transactions.groupby(["Дата операции", "Сумма платежа"])
+            .sum()
+            .reset_index()
+        )
 
-        total_spending = grouped_transactions['Сумма платежа'].sum()
+        total_spending = grouped_transactions["Сумма платежа"].sum()
 
-        return pd.DataFrame({'Категория': [category],
-                              'Общая сумма трат': [total_spending]
-                             })
+        return pd.DataFrame(
+            {"Категория": [category], "Общая сумма трат": [total_spending]}
+        )
 
     except Exception as e:
         print(f"Ошибка: {e}")
         return None
 
-file_path = '../data/operations.xlsx'
-category = 'Покупки'
+
+file_path = "../data/operations.xlsx"
+category = "Покупки"
 date = None
 
 result = spending_by_category(file_path, category, date)
--- src/services.py
+++ src/services.py
@@ -1,23 +1,26 @@
-from pprint import pprint
+
+import logging
 
 import pandas as pd
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    filename="../logs/services.log",
-                    filemode="w",
-                    encoding="utf-8",
-                    format="%(asctime)s %(name)s %(levelname)s %(message)s")
 
-logger = logging.getLogger('get_transfers')
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="../logs/services.log",
+    filemode="w",
+    encoding="utf-8",
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+)
 
-def get_transfers(file_path):
-    logger.info('Читаем данные из файла Excel')
+logger = logging.getLogger("get_transfers")
+
+
+def get_transfers(file_path: str):
+    '''Функция возвращает JSON со всеми транзакциями, которые относятся к переводам физлицам.'''
+    logger.info("Читаем данные из файла Excel")
     df = pd.read_excel(file_path)
 
     logger.info('Ищем в столбце "Описание операции" имена физлиц с точкой')
-    transfers_with_name = df[df['Описание'].str.contains(r'\b\w\.', case=False)]
+    transfers_with_name = df[df["Описание"].str.contains(r"\b\w\.", case=False)]
 
-    logger.info('Возвращаем результат в виде JSON-ответа')
-    return transfers_with_name.to_dict(orient='records')
-
-
+    logger.info("Возвращаем результат в виде JSON-ответа")
+    return transfers_with_name.to_dict(orient="records")
